1d8052db2509ee6be5d8ddae25fa2863146a378a
Henry Corredor González <78055527+henrycorredor7570@users.noreply.github.com>
2023-12-01 18:32:23 -0500

    Update PersonaDaoImpl.java

--- src/main/java/mx/com/gm/capadatos/PersonaDaoImpl.java
+++ src/main/java/mx/com/gm/capadatos/PersonaDaoImpl.java
@@ -11,7 +11,7 @@ import org.apache.logging.log4j.Logger;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public class PersonaDaoImpl implements PersonaDao {
+public class PersonaDaoImpl implements PersonaDao{
 
     Logger log = LogManager.getRootLogger();
 
